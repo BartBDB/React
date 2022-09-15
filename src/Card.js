50ea542277dcb1f506e163e7e1432fa8c6a5d6be
Bart de Boer <bartdb2001@gmail.com>
2022-09-15 15:05:37 +0200

    first commit

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+const Card = () => {
+    return(
+        <article>
+            <header>
+                <h2>eerste kaart</h2>
+            </header>
+            <section>
+                lorem ipsum nogwat nogwat nogwat
+            </section>
+        </article>
+    )
+}
+
+export default Card;
